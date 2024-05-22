ac018aef6f1545d4e96faa4bdab6cf86b9574dbe
Kaushik <roykaushik354@gmail.com>
2024-05-22 13:25:20 +0530

    initial commit

--- hh.py
+++ hh.py
@@ -1,1 +1,8 @@
 import time
+import FreeSimpleGUI as sg
+label=sg.Text("Type in a To-Do")
+input_box=sg.InputText()
+add_button=sg.Button("ADD")
+window=sg.Window('my to do app',layout=[[label],[input_box,add_button]])
+window.read()
+window.close()
